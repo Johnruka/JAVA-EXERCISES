80dcb1ef548483ff1b5657687dbb77ae4b736fcb
John Baptist Rukarugahieguru <rukaruga@yahoo.co.uk>
2024-09-13 12:24:31 +0200

    printed

--- src/main/java/se/lexicon/exercises/PrintNames.java
+++ src/main/java/se/lexicon/exercises/PrintNames.java
@@ -1,4 +1,12 @@
 package se.lexicon.exercises;
 
 public class PrintNames {
+
+    public static void main(String[] args) {
+        String firstname = "David";
+        System.out.println("Firstname: " + firstname);
+
+        String lastname = "Marcus";
+        System.out.println("Firstname Lastname: " + firstname.concat(" ").concat(lastname));
+    }
 }
